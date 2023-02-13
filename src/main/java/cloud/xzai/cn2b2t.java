a58e76853d9f5ec5cf360dbf10f13d049f4621dc
Xzai <98635300+XzaiCloud@users.noreply.github.com>
2023-02-13 13:08:08 +0800

    首次提交

---
+++ src/main/java/cloud/xzai/cn2b2t.java
@@ -0,0 +1,17 @@
+package cloud.xzai;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class cn2b2t extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
