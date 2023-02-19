81af77b051904c7428d0e4fd889ecb8165f668f5
Xzai <xzai0313@qq.com>
2023-02-19 12:15:51 +0800

    修改文件

--- src/main/java/cloud/xzai/cn2b2t.java
+++ src/main/java/cloud/xzai/cn2b2t.java
@@ -1,17 +1,33 @@
 package cloud.xzai;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class cn2b2t extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("hello")) {
+            Player player = (Player) sender;
+            player
+                .sendMessage("你好！这是一个测试插件，你的id是："
+                             + player.getName() + "\n生命值是："
+                               + player.getHealth() + "\n等级是：" + player.getLevel());
+            return true;
+        }
+        return false;
+    }
+
 }
