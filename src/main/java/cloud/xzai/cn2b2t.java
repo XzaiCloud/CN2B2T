e8b877606bb581c74867732b62f677fde111b0be
Xzai <98635300+XzaiCloud@users.noreply.github.com>
2023-02-19 18:39:44 +0800

    修改文件

--- src/main/java/cloud/xzai/cn2b2t.java
+++ src/main/java/cloud/xzai/cn2b2t.java
@@ -1,33 +1,57 @@
 package cloud.xzai;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+//作者：Xzai
+//介绍：依托答辩
 
 public final class cn2b2t extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getLogger().info("插件已加载");
     }
 
     @Override
     public void onDisable() {
+        getLogger().info("插件已卸载");
     }
 
-    @Override
+    public void consoleLog(String logInfo) {
+        CommandSender sender = Bukkit.getConsoleSender();
+        sender.sendMessage(logInfo);
+    }
+
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("hello")) {
-            Player player = (Player) sender;
-            player
-                .sendMessage("你好！这是一个测试插件，你的id是："
-                             + player.getName() + "\n生命值是："
-                               + player.getHealth() + "\n等级是：" + player.getLevel());
-            return true;
+        String playerName;
+        String info;
+        String commandName = command.getName();
+        Player player = (Player) sender;
+        if (sender.isOp() && commandName.equalsIgnoreCase("test")) {
+            playerName = player.getName();
+            info = "你好!管理员<" + playerName + ">";
+            player.sendMessage(info);
+            player.chat("我输入了命令/test,这插件太好用了吧！");
+            consoleLog(ChatColor.BLUE + "管理员<" + playerName + ">输入了一条命令:" + commandName);
+            consoleLog(ChatColor.BLUE + "服务器发给管理员<" + playerName + ">了一条消息:" + info);
+        } else if (commandName.equalsIgnoreCase("info")) {
+            playerName = player.getName();
+            info = "你好!玩家<" + playerName + ">";
+            player.sendMessage(info);
+            consoleLog(ChatColor.BLUE + "玩家<" + playerName + ">输入了一条命令:" + commandName);
+            consoleLog(ChatColor.BLUE + "服务器发给<" + playerName + ">了一条消息:" + info);
+        } else {
+            playerName = player.getName();
+            info = "玩家<" + playerName + ">执行了一条命令，但是出错了";
+            player.sendMessage(info);
+            consoleLog(ChatColor.BLUE + "玩家<" + playerName + ">输入了一条命令:" + commandName);
+            consoleLog(ChatColor.BLUE + "玩家<" + playerName + ">执行了一条命令，但是出错了");
         }
-        return false;
+        return true;
     }
-
 }
